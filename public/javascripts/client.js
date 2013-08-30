8e63711968562ff480554ebd313681acb0069c6a
Ben Myers <bmyers@appnexus.com>
2013-08-30 11:38:48 -0400

    Prettified, added styles and better HTML

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	var canvas = $("#canvas");
+	var form = $("#menu form");
 	var textField = $("#sticky-message");
 	var textColorDropDown = $("#sticky-text-color");
 	var paperColorDropDown = $("#sticky-paper-color");
@@ -9,16 +10,30 @@ $(document).ready(function() {
 	var board = new StickyBoard(io.connect());
 	$(board.getRoot()).appendTo(canvas);
 
-	createStickyButton.on('click', function(evt) {
-		board.createSticky({
-			text: textField.val(),
-			x: Math.floor(20 + (canvas.width() - 40) * Math.random()),
-			y: Math.floor(5 + (canvas.height() - 80) * Math.random()),
-			textColor: textColorDropDown.val(),
-			pinColor: pinColorDropDown.val(),
-			paperColor: paperColorDropDown.val()
-		});
-		textField.val("");
-		evt.preventDefault();
+	form.on('submit', function() {
+		createSticky();
+		return false;
 	});
+	textField.on('keypress', function(evt) {
+		if(evt.which === 13) {
+			createSticky();
+			setTimeout(function() {
+				textField.val("");
+			}, 0);
+		}
+	})
+
+	function createSticky() {
+		if(textField.val().trim() !== "") {
+			board.createSticky({
+				text: textField.val(),
+				x: Math.floor(20 + (canvas.width() - 40) * Math.random()),
+				y: Math.floor(5 + (canvas.height() - 80) * Math.random()),
+				textColor: textColorDropDown.val(),
+				pinColor: pinColorDropDown.val(),
+				paperColor: paperColorDropDown.val()
+			});
+			textField.val("");
+		}
+	}
 });
