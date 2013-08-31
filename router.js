b5949165d2303fd15b9e3c5a53062134d0eec9ec
Ben Myers <bmyers@appnexus.com>
2013-08-31 11:51:44 -0400

    Clean up and removing unused files

--- router.js
+++ router.js
@@ -1,11 +1,4 @@
 function renderIndex(req, res) {
-	/*if(req.session.count) {
-		req.session.count++;
-	}
-	else {
-		req.session.count = 1;
-	}
-	console.log(req.session.count + " visits");*/
 	res.render('index.jade', {});
 }
 
