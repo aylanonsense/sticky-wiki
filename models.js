64286a7a9d77741af64cc66996efcbd86bfdb530
Ben Myers <bmyers@appnexus.com>
2013-08-31 11:07:23 -0400

    Stickies now persisted using MongoStore

--- models.js
+++ models.js
@@ -1,48 +1,18 @@
 var mongoose = require('mongoose');
-var crypto = require('crypto');
-var User;
-var emailValidators;
 
-function isStringDefined(str) {
-	return str && str.length;
-}
-function isStringNotBlank(str) {
-	return str.length > 0;
-}
-function isValidEmailAddress(emailAddress) {
-    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-    return pattern.test(emailAddress);
-}
-
-emailValidators = [
-	{ validator: isStringDefined, msg: 'blank' },
-	{ validator: isStringNotBlank, msg: 'blank' },
-	{ validator: isValidEmailAddress, msg: 'invalid' }
-];
-
-var userSchema = new mongoose.Schema({
-	dateCreated: { type: Date, default: Date.now },
-	username: { type: String, index: { unique: true } },
-	email: { type: String, validate: emailValidators, index: { unique: true }},
-	hashedPassword: { type: String, required: true },
-	salt: { type: String, required: true },
-	isAdmin: { type: Boolean, default: false }
+var stickySchema = new mongoose.Schema({
+	text: { type: String },
+	x: { type: Number },
+	y: { type: Number },
+	textColor: { type: String },
+	paperColor: { type: String },
+	pinColor: { type: String },
+	rotation: { type: Number },
+	lastModified: { type: Date, default: Date.now }
 });
-userSchema.virtual('password').get(function() {
-	return this.hashedPassword;
-}).set(function(password) {
-	this.salt = crypto.createHmac('sha1', 'nosaltreqd').update(Math.random() + '_' + this.dateCreated.getTime()).digest('hex');
-	this.hashedPassword = this.hashPassword(password);
-});
-userSchema.virtual('id').get(function() {
+stickySchema.virtual('id').get(function() {
 	return this._id;
 })
-userSchema.method('authenticate', function(password) {
-	return this.hashPassword(password) === this.hashedPassword;
-});
-userSchema.method('hashPassword', function(password) {
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
-});
-User = mongoose.model('User', userSchema);
+var Sticky = mongoose.model('Sticky', stickySchema);
 
-exports.User = User;
+exports.Sticky = Sticky;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var models = require('./models');
+var Sticky = models.Sticky;
+
 function StickyServer() {
 	this._room = 'sticky_board_' + (this.NEXT_ROOM_ID++);
 	this._nextClientId = 1;
-	this._nextStickyId = 0;
 }
 StickyServer.prototype.NEXT_ROOM_ID = 1;
 StickyServer.prototype.addConnection = function(conn) {
@@ -21,30 +23,85 @@ StickyServer.prototype.addConnection = function(conn) {
 	console.log("Client " + clientId + " connected!");
 };
 StickyServer.prototype._sendAllStickies = function(conn) {
-	conn.io.emit('draw_stickies', []);
+	Sticky.find(function(err, stickyRecords) {
+		if(err) {
+			console.log("Error retrieving all stickies:", err);
+		}
+		else {
+			var stickies = [];
+			stickyRecords.forEach(function(stickyRecord) {
+				stickies.push({
+					id: stickyRecord.id,
+					text: stickyRecord.text,
+					x: stickyRecord.x,
+					y: stickyRecord.y,
+					textColor: stickyRecord.textColor,
+					paperColor: stickyRecord.paperColor,
+					pinColor: stickyRecord.pinColor
+				});
+			});
+			conn.io.emit('draw_stickies', stickies); //TODO sort by last modified
+		}
+	});
 };
 StickyServer.prototype._handleStickyCreateRequest = function(conn, seqNum, sticky) {
 	sticky = {
-		id: this._nextStickyId++,
 		text: sticky.text,
 		x: sticky.x,
 		y: sticky.y,
 		textColor: sticky.textColor,
-		pinColor: sticky.pinColor,
 		paperColor: sticky.paperColor,
+		pinColor: sticky.pinColor,
 		rotation: Math.round(30 * Math.random() - 15)
 	};
+	
+	//save the sticky to the db
+	var stickyRecord = new Sticky({
+		text: sticky.text,
+		x: sticky.x,
+		y: sticky.y,
+		textColor: sticky.textColor,
+		paperColor: sticky.paperColor,
+		pinColor: sticky.pinColor,
+		rotation: sticky.rotation
+	});
+	stickyRecord.save(function(err) {
+		if(err) {
+			console.log('Sticky "' + sticky.text + '" could NOT be saved!');
+		}
+		else {
+			console.log('Sticky "' + sticky.text + '" saved!');
+		}
+	});
+	sticky.id = stickyRecord.id;
+
 	//send sticky to all clients
 	conn.io.emit('replace_sticky', { seqNum: seqNum, sticky: sticky });
 	conn.io.room(this._room).broadcast('draw_sticky', sticky);
 };
 StickyServer.prototype._handleStickyMoveRequest = function(conn, stickyId, x, y) {
-	var msg = {
-		stickyId: stickyId,
-		x: x,
-		y: y
-	};
-	conn.io.room(this._room).broadcast('move_sticky', msg);
+	var self = this;
+	Sticky.findOne({ _id: stickyId }, function(err, stickyRecord) {
+		if(err || !stickyRecord) {
+			console.log("Could not find sticky with id of " + stickyId);
+		}
+		else {
+			var oldX = stickyRecord.x;
+			var oldY = stickyRecord.y;
+			stickyRecord.x = x;
+			stickyRecord.y = y;
+			stickyRecord.lastModified = Date.now();
+			stickyRecord.save(function(err) {
+				if(err) {
+					console.log('Could not move sticky "' + stickyRecord.text + '" to ' + x + ',' + y);
+					conn.io.emit('move_sticky', { stickyId: stickyId, x: oldX, y: oldY});
+				}
+				else {
+					conn.io.room(self._room).broadcast('move_sticky', { stickyId: stickyId, x: x, y: y});
+				}
+			});
+		}
+	});
 };
 
 module.exports = StickyServer;
